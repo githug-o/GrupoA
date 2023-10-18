9bfdcf61b12cf5f9f8f6cff4ae77314fec924c3e
Hugo Oliveira <146291968+githug-o@users.noreply.github.com>
2023-10-18 19:09:21 -0300

    Update atividade_18-10.py

--- atividade_18-10.py
+++ atividade_18-10.py
@@ -1,5 +1,10 @@
 import csv
+
+
 #escrevam o cógido abaixo do comentário com seu nome
+#variaveis: tarefa, descricao, data, tipodetarefa
+#objeto = agenda
+#funcoes = imprimir_atrasadas(), cadastrar_tarefa(), editar_tarefa(), criar_arquivo(), ler_csv()
 
 #DaNasser
 
