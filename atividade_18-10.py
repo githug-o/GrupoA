10962d021178dd2b9cfd2b12273a62981f404afe
DaNasser <dannasser2007@gmail.com>
2023-10-18 19:45:31 -0300

    Update atividade_18-10.py

--- atividade_18-10.py
+++ atividade_18-10.py
@@ -7,6 +7,21 @@ import csv
 #funcoes = imprimir_atrasadas(), cadastrar_tarefa(), editar_tarefa(), criar_arquivo(), ler_csv()
 
 #DaNasser
+def editar_tarefa(agenda):
+    pesquisa = input("Digite o nome da sua tarefa que deseja atualizar: ")
+    for  tarefa in agenda:
+        if tarefa ['Tarefa'] == pesquisa:
+            tarefa['Tarefa'] = input("Digite o nome da tarefa: ")
+            tarefa['Lista'] = input("Digite a lista da sua tarefa: ")
+            dia = input("Digite o dia de expiração: ")
+            mes = input("Digite o mês de expiração: ")
+            ano = input("Digite o ano de expiração: ")
+            tarefa ['Data'] = datetime(ano,mes,dia)
+            tarefa ['Status'] = input("Digite o status da atividade: ")
+        else: 
+            print("Tarefa não encontrada!")
+    def criar_arquivo():
+        
 
 
 
@@ -19,6 +34,5 @@ import csv
 
 
 #BernardoGuerino
-
 
 
