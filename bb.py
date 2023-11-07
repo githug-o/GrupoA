a654731ff6ea37e13acb4a1536827bd054b8e943
BernardoGuerino <bernardo.coelho.guerino@gmail.com>
2023-11-07 20:08:45 -0300

    Create bb.py

---
+++ bb.py
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <style>
+    body {
+       color: rgb(0, 0, 0);
+}
+  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<h1></h1> 
+  <h2> Preencha o formulário abaixo para se cadastrar</h2><br />
+<body>
+    <legend>Dados de login</legend>
+    <table cellspacing="10">
+     <tr>
+      <td>
+       <label for="nome">Nome: </label>
+      </td>
+      <td align="left">
+       <input type="text" name="nome">
+      </td>
+     </tr>
+     <tr>
+      <td>
+        <label for="email">E-mail: </label>
+      </td>
+      <td align="left">
+       <input type="text" name="email">
+
+      </td>
+     </tr>
+     <tr>
+      <td>
+       <label for="pass">Senha: </label>
+      </td>
+      <td align="left">
+       <input type="password" name="pass">
+      </td>
+     </tr>
+     <tr>
+      <td>
+       <label for="passconfirm">Confirme a senha: </label>
+      </td>
+      <td align="left">
+       <input type="password" name="passconfirm">
+      </td>
+     </tr>
+    </table>
+   </fieldset>
+   <br />
+   <input type="submit">
+   <input type="reset" value="Limpar">
+   </form>
+    
+</body>
+</html>
